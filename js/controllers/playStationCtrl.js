ac71bc1cd27213ffafb1c6ea6ba029b33e4aa86e
goverdhan1 <goverdhan.k@gmail.com>
2016-08-13 07:42:45 +0530

    http-server

--- js/controllers/playStationCtrl.js
+++ js/controllers/playStationCtrl.js
@@ -6,6 +6,7 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
         var promise;
         var systems = playStationService.getSystems();
         var customers = playStationService.getCustomers();
+        var waitingList = playStationService.getWaitingList();
 
         if (systems === null) {
             playStationService.setSystems();
@@ -15,11 +16,16 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
             playStationService.setCustomers();
             customers = playStationService.getCustomers();
         }
+        if (waitingList === null) {
+            playStationService.setWaitingList();
+            waitingList = playStationService.getWaitingList();
+        }
 
+        scope.numberOfSystems= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         scope.systems = systems;
         scope.customers = customers;
+        scope.waitingList = waitingList;
         scope.master = [];
-        scope.waitList = [];
 
         scope.activeCustomers = [];
         scope.inActiveCustomers = [];
@@ -47,28 +53,88 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
         var priceList = [
             [10, 20],
             [30, 40],
-            [50, 60]
+            [40, 50]
         ];
 
-        scope.enableStart = function(obj, index) {
-            if (!obj.customer.name && !obj.members.count && !obj.duration.hours) {
-                obj.startBtn.disable = true;
-            } else {
-                obj.startBtn.disable = false;
+        var reset = function(systemNumber) {
+            return {
+                systemNumber: systemNumber,
+                customer: {
+                    name: null,
+                    disable: false
+                },
+                members: {
+                    count: 1,
+                    disable: false
+                },
+                duration: {
+                    hours: 1,
+                    disable: false
+                },
+                startBtn: {
+                    disable: true,
+                    show: true
+                },
+                continueBtn: {
+                    disable: true,
+                    show: false
+                },
+                editBtn: {
+                    disable: true,
+                    show: false
+                },
+                stopBtn: {
+                    disable: true,
+                    show: false
+                },
+                cancelBtn: {
+                    disable: true,
+                    show: false
+                },
+                updateBtn: {
+                    disable: true,
+                    show: false
+                },
+                invoiceDetails: [],
+                startTime: {
+                    value: null,
+                    show: false
+                },
+                endTime: {
+                    value: null,
+                    show: false
+                },
+                timeStamp: null,
+                amount: null,
+                status: false,
+                editMode: false,
+                cancelGameBtn: {
+                    disable: true,
+                    show: false
+                },
+                timeFinish: false,
+                editStartTimePanel: {
+                    disable: true,
+                    show: false
+                }
             }
         };
+        scope.waiting = new reset("null");
 
-        scope.start = function(obj1, index) {
-            var obj = reset(index)
+        scope.systemStatus= function(index)
+        {
+            var flag=false;
+            angular.forEach(scope.systems, function(system){
+                if(system.systemNumber==index && system.status){
+                    flag=true;
+                }
+            })
+            return flag;
+        }
 
-            obj.customer.name = obj1.customer.name;
-            obj.members.count = obj1.members.count;
-            obj.systemNumber = parseInt(index)+1;
-            obj.duration.hours = obj1.duration.hours;
-
-            scope.waiting = reset(null);
-
+        scope.start = function(obj, index) {
             obj.status = true;
+            obj.amount = null;
             date = new Date();
             obj.timeStamp = date;
             laterTime = new Date(date.getTime() + obj.duration.hours * 30 * 60 * 1000);
@@ -107,25 +173,23 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
             for (var i = 0; i < obj.invoiceDetails.length; i++) {
                 obj.amount += obj.invoiceDetails[i].amt;
             }
-            scope.systems[index]= obj;
+            scope.systems.push(obj);
             $window.localStorage.setItem("sys", JSON.stringify(scope.systems));
             promise = timeout(function() {
 
-                obj.timeFinish = true;
-                obj.members.disable = false;
-                obj.duration.disable = false;
-                obj.continueBtn.disable = false;
-                obj.continueBtn.show = true;
+                scope.systems[index].timeFinish = true;
+                scope.systems[index].members.disable = false;
+                scope.systems[index].duration.disable = false;
+                scope.systems[index].continueBtn.disable = false;
+                scope.systems[index].continueBtn.show = true;
 
-                obj.editBtn.disable = true;
-                obj.editBtn.show = false;
+                scope.systems[index].editBtn.disable = true;
+                scope.systems[index].editBtn.show = false;
 
                 document.getElementById("play" + index).play();
-                scope.systems[index]= obj;
                 $window.localStorage.setItem("sys", JSON.stringify(scope.systems));
             }, obj.timeInterval);
-
-
+            scope.waiting = new reset("null");
         };
 
         scope.enableEdit = function(obj, index) {
@@ -301,55 +365,45 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
 
 
         scope.calculateAmount=function(){
-
-            if(scope.waiting.customer.name!= null && scope.waiting.members.count!= null && scope.waiting.duration.hours!= null ){
+            if(scope.waiting.members.count && scope.waiting.duration.hours ){
                 scope.waiting.amount = priceList[scope.waiting.members.count - 1][scope.waiting.duration.hours - 1]                
-            }else{
-                scope.waiting.amount =null;
             }
         };
 
         scope.enableWait = function() {
-            if (scope.waiting.customer.name != null && scope.waiting.members.count != null && scope.waiting.duration.hours != null) {
+            if (!scope.waiting.customer.name || !scope.waiting.members.count || !scope.waiting.duration.hours) {
                 return false;
             } else {
                 return true;
             }
 
         }
-        scope.wait = function() {
+         scope.enableStart = function() {
+            var flag = true;
+            if (scope.waiting.customer.name && scope.waiting.systemNumber!="null" && scope.waiting.members.count && scope.waiting.duration.hours) {
+                flag = false;
+            } 
+            return flag;
+        }
+        scope.wait = function(obj) {
+console.log(obj)
+            scope.waitingList.push(obj);
+            $window.localStorage.setItem("waitingList", JSON.stringify(scope.waitingList));
 
-            scope.waitList.push({
-                customer: {
-                    name: scope.waiting.customer.name
-                },
-                members: {
-                    count: scope.waiting.members.count
-                },
-                duration: {
-                    hours: scope.waiting.duration.hours
-                }
-            });
-            scope.waiting.customer.name = null;
-            scope.waiting.members.count = null;
-            scope.waiting.duration.hours = null;
 
         };
         scope.assignSystem = function(index, sysNum) {
-            scope.systems[sysNum].customer.name = scope.waitList[index].customer.name;
-            scope.systems[sysNum].members.count = scope.waitList[index].members.count;
-            scope.systems[sysNum].duration.hours = scope.waitList[index].duration.hours;
-            scope.waitList.splice(index, 1);
-
+            scope.waitingList.splice(index, 1);
+            $window.localStorage.setItem("waitingList", JSON.stringify(scope.waitingList));
         };
 
         scope.removeWaiting = function(index) {
-            scope.waitList.splice(index, 1);
+            scope.waitingList.splice(index, 1);
+            $window.localStorage.setItem("waitingList", JSON.stringify(scope.waitingList));
         };
 
         scope.cleanSystems = function() {
             $window.localStorage.removeItem("sys")
-            alert("remove systems");
         };
 
         scope.cleanCustomers = function() {
@@ -359,70 +413,6 @@ app.controller('playStationCtrl', ['$scope', "$timeout", "$window", "playStation
         var pad2 = function(number) {
             return (number < 10 ? '0' : '') + number
         };
-        var reset = function(systemNumber) {
-            return {
-                systemNumber: systemNumber,
-                customer: {
-                    name: null,
-                    disable: false
-                },
-                members: {
-                    count: 1,
-                    disable: false
-                },
-                duration: {
-                    hours: 1,
-                    disable: false
-                },
-                startBtn: {
-                    disable: true,
-                    show: true
-                },
-                continueBtn: {
-                    disable: true,
-                    show: false
-                },
-                editBtn: {
-                    disable: true,
-                    show: false
-                },
-                stopBtn: {
-                    disable: true,
-                    show: false
-                },
-                cancelBtn: {
-                    disable: true,
-                    show: false
-                },
-                updateBtn: {
-                    disable: true,
-                    show: false
-                },
-                invoiceDetails: [],
-                startTime: {
-                    value: null,
-                    show: false
-                },
-                endTime: {
-                    value: null,
-                    show: false
-                },
-                timeStamp: null,
-                amount: null,
-                status: false,
-                editMode: false,
-                cancelGameBtn: {
-                    disable: true,
-                    show: false
-                },
-                timeFinish: false,
-                editStartTimePanel: {
-                    disable: true,
-                    show: false
-                }
-            }
-        };
-        scope.waiting = reset(null);
 
     }
 ]);
